c13feb7c00f77415c1e529b6beb5ba9457d2b741
Lucas Custódio <86917624+mamutthe@users.noreply.github.com>
2022-12-25 16:31:22 +0000

    Pushing project to github

---
+++ components/Pearphone.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+
+const Pearphone = () => {
+  return (
+    <div className="relative">
+    <Image
+      alt="The Slap Update Image"
+      src="https://i.ibb.co/cwDsGVB/4k.png"
+      width={800}
+      height={800}
+    />
+  </div>
+  )
+}
+
+export default Pearphone;
